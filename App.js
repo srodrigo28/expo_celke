d9567287ee865707fff1c25b70d6ca654162788a
Sebastiao Rodrigo <rodrigoexer1@gmail.com>
2022-02-24 23:13:28 -0300

    update

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello React Expo</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignContent: "center"
   },
 });
--- README.md
+++ README.md
@@ -4,9 +4,14 @@
 * https://celke.com.br/clms/aula-curso/view/4403
 
 ## Comandos
+* Caso não tenha instalado o react native
+    * npm i -g react-native
 * Criação do projeto
     * npx expo init app
 * Entrar na pasta
     * cd app
 * Iniciar projeto
     * expo start
+* Navigation
+    * https://reactnavigation.org/docs/getting-started/
+    * expo install react-native-screens react-native-safe-area-context
